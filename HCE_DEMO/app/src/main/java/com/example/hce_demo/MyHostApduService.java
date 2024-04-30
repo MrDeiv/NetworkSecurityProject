a44e6f2bb507d50ca9d78510205f3da4b7c38ae7
atonio <atoniocampus276@gmail.com>
2024-05-01 01:38:54 +0200

    HCE clean

--- HCE_DEMO/app/src/main/java/com/example/hce_demo/MyHostApduService.java
+++ HCE_DEMO/app/src/main/java/com/example/hce_demo/MyHostApduService.java
@@ -10,36 +10,6 @@ import java.util.Arrays;
 public class MyHostApduService extends HostApduService {
 
     private int messageCounter = 0;
-
-    @Override
-    public byte[] processCommandApdu(byte[] apdu, Bundle extras) {
-        Log.i("HCE", "Receiving");
-        if (Arrays.equals(CUSTOM_AID, apdu)){
-            Log.i("HCE", "Application selected");
-            return getWelcomeMessage();
-        }
-        else {
-            Log.i("HCE", "Received: " + new String(apdu));
-            return getNextMessage();
-        }
-    }
-
-    private byte[] getWelcomeMessage() {
-        return "Hello Desktop!".getBytes();
-    }
-
-    private byte[] getNextMessage() {
-        return ("Message from HCE: CON LA SABBIA " + messageCounter++).getBytes();
-    }
-
-    private boolean selectAidApdu(byte[] apdu) {
-        return apdu.length >= 2 && apdu[0] == (byte) 0 && apdu[1] == (byte) 0xa4;
-    }
-
-    @Override
-    public void onDeactivated(int reason) {
-        Log.i("HCE", "Deactivated: " + reason);
-    }
     private static final byte[] CUSTOM_AID = {
             (byte)0x00,  // CLA
             (byte)0xa4,  // INS
@@ -50,4 +20,22 @@ public class MyHostApduService extends HostApduService {
             (byte)0xF0, (byte)0x01, (byte)0x02, (byte)0x03, (byte)0x04, (byte)0x05, (byte)0x06,
             (byte)0x00   // LE
     };
+
+    @Override
+    public byte[] processCommandApdu(byte[] apdu, Bundle extras) {
+        if (Arrays.equals(CUSTOM_AID, apdu)){
+            Log.i("HCE", "Application selected");
+            return "ACK".getBytes();
+        }
+        else {
+            Log.i("HCE", "Received: " + new String(apdu));
+            return ("Message from HCE: CON LA SABBIA " + messageCounter++).getBytes();
+        }
+    }
+
+    @Override
+    public void onDeactivated(int reason) {
+        Log.i("HCE", "Deactivated: " + reason);
+    }
+
 }
